a8910abf1a9d9597f12452a8e9325ef72d9aee68
xylon <xylonyoung@live.com>
2020-06-13 17:35:15 +0800

    Update request.js

--- api/request.js
+++ api/request.js
@@ -1,75 +1,73 @@
-const baseURL = 'http://47.107.144.252:8003'
-
-const token = uni.getStorageSync('token')
+const baseURL = 'http://47.107.144.252:8003';
 
 function request(options) {
-	return new Promise((resolve, reject) => {
-		uni.request({
-			url: baseURL + options.url,
-			header: {
-				'Content-Type': 'application/json;charset=UTF-8',
-				'X-Auth-Token': token,
-			},
-			data: options.data,
-			method: options.method,
-			dataType: options.dataType,
-			responseType: "text",
-			success(res) {
-				if (res.data.code === 0) {
-					resolve(res.data)
-				} else {
-					reject(res.data)
-					let message = res.data.message ? res.data.message : res.data
-					uni.showToast({
-						title: message,
-						icon: 'none',
-					})
-				}
-			},
-			fail(err) {
-				uni.showToast({
-					title: '网络错误！',
-					icon: 'none'
-				})
-			},
-		})
-	})
+    const token = uni.getStorageSync('token');
+    return new Promise((resolve, reject) => {
+        uni.request({
+            url: baseURL + options.url,
+            header: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                'X-Auth-Token': token,
+            },
+            data: options.data,
+            method: options.method,
+            dataType: options.dataType,
+            responseType: 'text',
+            success(res) {
+                if (res.data.code === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data);
+                    let message = res.data.message ? res.data.message : res.data;
+                    uni.showToast({
+                        title: message,
+                        icon: 'none',
+                    });
+                }
+            },
+            fail(err) {
+                uni.showToast({
+                    title: '网络错误！',
+                    icon: 'none',
+                });
+            },
+        });
+    });
 }
 
 function post(url, data, options = {}) {
-	options.url = url
-	options.data = data
-	options.method = 'POST'
-	return request(options)
+    options.url = url;
+    options.data = data;
+    options.method = 'POST';
+    return request(options);
 }
 
 function del(url, data, options = {}) {
-	options.url = url
-	options.data = data
-	options.method = 'DELETE'
-	return request(options)
+    options.url = url;
+    options.data = data;
+    options.method = 'DELETE';
+    return request(options);
 }
 
 function put(url, data, options = {}) {
-	options.url = url
-	options.data = data
-	options.method = 'PUT'
-	return request(options)
+    options.url = url;
+    options.data = data;
+    options.method = 'PUT';
+    return request(options);
 }
 
 function get(url, data, options = {}) {
-	options.url = url
-	options.data = data
-	options.method = 'GET'
-	return request(options)
+    options.url = url;
+    options.data = data;
+    options.method = 'GET';
+    return request(options);
 }
 
 module.exports = {
-	baseURL,
-	token,
-	request,
-	post,
-	del,
-	put,
-	get
-}
+    baseURL,
+    request,
+    post,
+    del,
+    put,
+    get,
+};
