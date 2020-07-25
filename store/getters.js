402cebf13ded5e1aa7f96bc58ca4dac0d4d51219
xylon <xylonyoung@live.com>
2020-07-25 16:40:57 +0800

    update

--- store/getters.js
+++ store/getters.js
@@ -1,9 +1,6 @@
 const getters = {
 	token: state => state.user.token,
 	userInfo: state => state.user.userInfo,
-	userId: state => state.user.userInfo.id,
-	avatar: state => state.user.profile.avatarUrl,
-	nickName: state => state.user.profile.nickName,
 	profile: state => state.user.profile
 }
 export default getters
--- store/modules/user.js
+++ store/modules/user.js
@@ -1,114 +1,114 @@
 import { getLogin, putUserProfile, getUser } from '../../api/api.js'
 
 const state = {
-	token: '',
-	userInfo: {},
-	profile: {}
+  token: '',
+  userInfo: {},
+  profile: {},
+  registered: false,
 }
 
 const mutations = {
-	SET_TOKEN: (state, token) => {
-		state.token = token
-	},
-	SET_USER_INFO: (state, userInfo) => {
-		state.userInfo = userInfo
-	},
-	SET_PROFILE: (state, profile) => {
-		state.profile = profile
-	}
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_USER_INFO: (state, userInfo) => {
+    state.userInfo = userInfo
+  },
+  SET_PROFILE: (state, profile) => {
+    state.profile = profile
+  },
+  SET_REGISTERED: (state, registered) => {
+    state.registered = registered
+  },
 }
 
 const actions = {
-	// user login
-	login({ commit }) {
-		const token = uni.getStorageSync('token')
-		commit('SET_TOKEN', token)
-		getUser().then(res => {
-			const { data } = res
-			commit('SET_USER_INFO', data)
-		})
-	},
-	//weixin login
-	wxLogin({ dispatch, commit }) {
-		const token = uni.getStorageSync('token')
-		const getInfo = () => {
-			uni.getUserInfo({
-				success: response => {
-					const { userInfo } = response
-					commit('SET_PROFILE', userInfo)
-					console.log(userInfo)
-					putUserProfile(userInfo).then(() => {
-						getUser().then(res => {
-							const { data } = res
-							commit('SET_USER_INFO', data)
-						})
-					})
-				},
-				fail: () => {
-					console.log('未授权')
-				}
-			})
-		}
-		return new Promise(resolve => {
-			if (token) {
-				commit('SET_TOKEN', token)
-				getInfo()
-				resolve(true)
-			} else {
-				uni.login({
-					provider: 'weixin',
-					success: res => {
-						getLogin({
-							code: res.code
-						}).then(response => {
-							const { data } = response
-							commit('SET_TOKEN', data.token)
-							uni.setStorageSync('token', data.token)
-							getInfo()
-							resolve(res)
-						})
-					},
-					fail: err => {
-						console.log(err)
-						resolve(err)
-						uni.showModal({
-							title: '登录失败，请点击确认重新登录',
-							showCancel: false,
-							success: function(res) {
-								if (res.confirm) {
-									dispatch('wxLogin')
-								} else if (res.cancel) {
-									console.log('用户点击取消')
-								}
-							}
-						})
-					}
-				})
-			}
-		})
-	},
+  updateUser({ commit }) {
+    getUser().then(res => {
+      const { data } = res
+      commit('SET_USER_INFO', data)
+      if (data.profile) {
+        const profile = data.profile.__metadata
+        if (profile.phone && profile.room) {
+          commit('SET_REGISTERED', true)
+        }
+      }
+    })
+  },
+  // user login
+  login({ dispatch, commit }) {
+    const token = uni.getStorageSync('token')
+    commit('SET_TOKEN', token)
+    dispatch('updateUser')
+  },
+  //weixin login
+  wxLogin({ dispatch, commit }) {
+    const token = uni.getStorageSync('token')
+    const getInfo = () => {
+      uni.getUserInfo({
+        success: response => {
+          const { userInfo } = response
+          commit('SET_PROFILE', userInfo)
+          putUserProfile(userInfo).then(() => {
+            dispatch('updateUser')
+          })
+        },
+        fail: () => {
+          console.log('未授权')
+          dispatch('updateUser')
+        },
+      })
+    }
+    return new Promise(resolve => {
+      if (token) {
+        commit('SET_TOKEN', token)
+        getInfo()
+        resolve(true)
+      } else {
+        uni.login({
+          provider: 'weixin',
+          success: res => {
+            getLogin({
+              code: res.code,
+            }).then(response => {
+              const { data } = response
+              commit('SET_TOKEN', data.token)
+              uni.setStorageSync('token', data.token)
+              getInfo()
+              resolve(res)
+            })
+          },
+          fail: err => {
+            console.log(err)
+            resolve(err)
+            uni.showModal({
+              title: '登录失败，请点击确认重新登录',
+              showCancel: false,
+              success: function (res) {
+                if (res.confirm) {
+                  dispatch('wxLogin')
+                } else if (res.cancel) {
+                  console.log('用户点击取消')
+                }
+              },
+            })
+          },
+        })
+      }
+    })
+  },
 
-	register() {
-		return new Promise(resolve => {
-			if (state.profile.phone && state.profile.room) {
-				resolve(true)
-			} else {
-				resolve(false)
-			}
-		})
-	},
-
-	// user logout
-	logout() {
-		uni.clearStorageSync()
-		uni.reLaunch({
-			url: '../login/login'
-		})
-	}
+  // user logout
+  logout() {
+    uni.clearStorageSync()
+    uni.reLaunch({
+      url: '../login/login',
+    })
+  },
 }
 export default {
-	namespaced: true,
-	state,
-	mutations,
-	actions
+  namespaced: true,
+  state,
+  mutations,
+  actions,
 }
