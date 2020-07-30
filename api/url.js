1b2d27bd0e49495f3d9b110ee98261abac9ece12
xylon <xylonyoung@live.com>
2020-07-30 17:50:31 +0800

    update

--- api/url.js
+++ api/url.js
@@ -1,3 +1,4 @@
 export const baseURL = 'http://47.107.144.252:8007'
 export const imageURL = baseURL + '/uploads/images/'
+export const videoURL = baseURL + '/uploads/videos/'
 export const uploadURL = baseURL + '/upload'
