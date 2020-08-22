19afe991f8ea5edea69439ac1ca826a23a38792a
xylon <xylonyoung@live.com>
2020-08-22 18:14:52 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -6,7 +6,7 @@ export default {
       window.console &&
         window.console.log &&
         console.log(
-          '%cDream%c\n%cStudio',
+          '%c江门%c\n%c码农',
           'font-size:30px;color:white;background:black;border-radius:5px 5px 0 0;padding:10px 20px 0;',
           '',
           'font-size:30px;color:black;background:orange;border-radius:0 0 5px 5px;border:solid black;border-width: 0 15px 10px 15px;padding:0 5px;'
