9c94c93d4410a317efc1494c50e3428df8b0cd96
xylon <xylonyoung@live.com>
2020-08-27 15:38:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App'
 import uView from 'uview-ui'
 import store from './store'
-import { getProperty } from './utils/get-property'
-
-Vue.prototype.getProperty = getProperty
 
 Vue.use(uView)
 
